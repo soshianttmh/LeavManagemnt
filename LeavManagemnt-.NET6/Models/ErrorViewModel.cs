b4a265a3204796004f41df2e8df67532e231809b
soshiantmh <soshiantmhuni@gmail.com>
2022-10-29 17:23:47 +0330

    Add project files.

---
+++ LeavManagemnt-.NET6/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace LeavManagemnt_.NET6.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
